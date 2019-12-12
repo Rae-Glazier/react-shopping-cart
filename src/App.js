1bea0177dd66f01064f0da5fa77693c71899cb02
rae-glazier <glazier.home.life@gmail.com>
2019-12-12 07:54:21 -0600

    its completed..but im still getting errors?!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -19,6 +20,8 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider>
+				
 			<div className="App">
 				<Navigation cart={cart} />
 
@@ -30,6 +33,8 @@ function App() {
 					render={() => <ShoppingCart cart={cart} />}
 				/>
 			</div>
+
+			</CartContext.Provider>
 		</ProductContext.Provider>
 
 	);
